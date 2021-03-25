8f99e71057b520b1433adc63d9f69ac468c75167
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-03-25 15:17:21 +0200

    Removed special characters from author's name and updated its style

--- bookscraper/spiders/diverta_spider.py
+++ bookscraper/spiders/diverta_spider.py
@@ -1,6 +1,11 @@
 import scrapy
 from bookscraper.items import BookItem
 from scrapy_splash import SplashRequest
+import unicodedata
+
+
+def process_author_name(author):
+    return ' '.join(name.capitalize() for name in author.lower().split(' '))
 
 
 class DivertaSpider(scrapy.Spider):
@@ -30,6 +35,11 @@ class DivertaSpider(scrapy.Spider):
         author = response.css('h1 div::text').get().strip()
         if author is None or author == '':
             author = response.xpath("//td[contains(text(),'Autor')]/following-sibling::td/text()").get().strip()
+            author = unicodedata.normalize("NFKD", author)
+            author = process_author_name(author)
+        else:
+            author = unicodedata.normalize('NFKD', author)
+            author = process_author_name(author)
         book['author'] = author
         book['publisher'] = response.xpath("//b[starts-with(text(),'Editura')]/parent::span/text()").get().strip()
         number_of_pages = response.xpath("//td[contains(text(),'pagini')]/following-sibling::td/text()").get() \
