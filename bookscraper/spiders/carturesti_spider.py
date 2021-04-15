5f66600b45c69744f9f471c1f0abae23e7f59456
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-04-15 21:27:43 +0300

    Added method for building the offer

--- bookscraper/spiders/carturesti_spider.py
+++ bookscraper/spiders/carturesti_spider.py
@@ -2,6 +2,19 @@ import scrapy
 from bookscraper.items import BookItem
 from scrapy_splash import SplashRequest
 
+
+def get_offer(response):
+    price = response.xpath("concat(//span[contains(@class,'pret')]/text(),'',//span[@class='bani']/text())").get() \
+        .strip()
+    return {
+        'link': response.meta.get('link'),
+        'provider': 'Carturesti',
+        'price': float(price) if price is not None and '' else None,
+        'hasStock': True if response.css("span.stocText::text").get() != 'Indisponibil' else False,
+        'transportationCost': 19.90
+    }
+
+
 lua_script = """
     function main(splash)
         assert(splash:go(splash.args.url))
@@ -27,8 +40,9 @@ class CarturestiSpider(scrapy.Spider):
     # only for testing purposes. url will be passed through scrapyrt as a param
     def start_requests(self):
         url = f'https://carturesti.ro/product/search/Sapiens?page=1&id_product_type=26'
-        yield SplashRequest(url=url, dont_filter=True, callback=self.parse, args={'wait': 1,'forbidden_content_types': 'text/css,font/*',
-                                                                'filters': 'easylist'})
+        yield SplashRequest(url=url, dont_filter=True, callback=self.parse,
+                            args={'wait': 1, 'forbidden_content_types': 'text/css,font/*',
+                                  'filters': 'easylist'})
 
     def parse(self, response):
         books = response.css('div.cartu-grid-tile')
@@ -40,22 +54,14 @@ class CarturestiSpider(scrapy.Spider):
             book['imgUrl'] = b.css('div.productImageContainer img::attr(src)').get()
             individual_book_url = b.css('a::attr(href)').get()
             book_link = f'{base_url}{individual_book_url}'
-            book['offer'] = {
-                'link': book_link,
-                'provider': 'Carturesti',
-                'price': float(b.css('span.suma::attr(content)').get()) if b.css('span.suma::attr(content)').get()
-                else None,
-                'hasStock': True if b.css('div.productStock span::text').get() != 'Indisponibil' else False,
-                'transportationCost': 19.90
-            }
             yield SplashRequest(url=book_link,
                                 dont_filter=True,
                                 endpoint='execute',
                                 callback=self.parse_book_info,
-                                args={'lua_source': lua_script, 'wait': 0.5, 'images': 0,
+                                args={'lua_source': lua_script, 'wait': 1, 'images': 0,
                                       'forbidden_content_types': 'text/css,font/* ',
                                       'filters': 'easylist'},
-                                meta={'book': book})
+                                meta={'book': book, 'link': book_link})
 
     def parse_book_info(self, response):
         book = response.meta.get('book')
@@ -66,14 +72,8 @@ class CarturestiSpider(scrapy.Spider):
         book['isbn'] = response.xpath("//span[starts-with(text(),'ISBN')]/following-sibling::div/text()").get()
         cover_type = response.xpath("//span[starts-with(text(),'Tip')]/following-sibling::div/text()").get()
         book['coverType'] = cover_type.strip() if cover_type is not None else None
-
+        book['offer'] = get_offer(response)
         yield book
 
     def parse_simple_book_info(self, response):
-        b = BookItem()
-        price = response.xpath("concat(//span[@class='pret']/text(),'',//span[@class='bani']/text())").get().strip()
-        b['offer'] = {
-            'price': float(price),
-            'hasStock': True if response.css("span.stocText::text").get() != 'Indisponibil' else False
-        }
-        yield b
+        yield get_offer(response)
